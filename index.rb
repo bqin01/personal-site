be8079a62703331c13db9f2b4b83f406f40d2143
Bill Z. Qin <bzqin88@gmail.com>
2019-07-18 14:20:45 -0400

    darker theme adopted for contrast

--- index.rb
+++ index.rb
@@ -6,6 +6,10 @@ class Appl < Sinatra::Base
     subpages = File.open("data/subpages.json")
     @jsonsubpages = JSON.load(subpages)
     subpages.close()
+    @curpage = "Homepage"
     erb :index
+  end
+  get '/about' do
+    @curpage = "About"
   end
 end
