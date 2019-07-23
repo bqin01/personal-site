d21e85849b53fe22122e8178ed8edf0c6cbb0ee6
Bill Z. Qin <bzqin88@gmail.com>
2019-07-23 16:48:47 -0400

    experiences.erb, projects.erb

--- index.rb
+++ index.rb
@@ -16,4 +16,45 @@ class Appl < Sinatra::Base
     @curpage = "About"
     erb :about
   end
+  get '/experience' do
+    subpages = File.open("data/subpages.json")
+    @jsonsubpages = JSON.load(subpages)
+    subpages.close()
+    experience = File.open("data/experience.json")
+    @experiences = JSON.load(experience)
+    experience.close()
+    @curpage = "Experience"
+    erb :experience
+  end
+  get '/projects' do
+    subpages = File.open("data/subpages.json")
+    @jsonsubpages = JSON.load(subpages)
+    subpages.close()
+    projects = File.open("data/projects.json")
+    @projects = JSON.load(projects)
+    projects.close()
+    @curpage = "Projects"
+    erb :projects
+  end
+  get '/skills' do
+    subpages = File.open("data/subpages.json")
+    @jsonsubpages = JSON.load(subpages)
+    subpages.close()
+    @curpage = "Skills"
+    erb :skills
+  end
+  get '/contact' do
+    subpages = File.open("data/subpages.json")
+    @jsonsubpages = JSON.load(subpages)
+    subpages.close()
+    @curpage = "Contact"
+    erb :contact
+  end
+  get '/:other' do
+    subpages = File.open("data/subpages.json")
+    @jsonsubpages = JSON.load(subpages)
+    subpages.close()
+    @curpage = "Other"
+    erb :page404
+  end
 end
