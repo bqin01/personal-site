c0435d229eae7bb32f2f805585a61a35387c8c7f
Bill Z. Qin <bzqin88@gmail.com>
2019-08-06 19:59:31 -0400

    about.blade.php

---
+++ resources/views/about.blade.php
@@ -0,0 +1,24 @@
+@extends('layout')
+@section('css',"/styles/about.css")
+@section('navbar')
+  @parent
+@endsection
+@section('content')
+  <div id = "background">
+    <div id = "about-box">
+      <h1>Bill Z. Qin</h1>
+      <h2>
+        I am a student born on April 9, 2001 in Toronto, ON, Canada interested in algorithm design, full stack app development, mathematics,
+        video directing/editing, game design, game theory, the sciences of creativity, and the importance of Internet meme culture.
+      </h2>
+      <h2>
+        After attending both grade school and middle school in Toronto, I was accepted to attend Phillips Academy in Andover, MA and matriculated
+        in September, 2015. There, I became interested in the video arts, fencing, and American law, while being able to continue pursuing
+        mathematics and computer science.
+      </h2>
+      <h2>
+        Other interests I have include badminton, table tennis, playing the piano, the clarinet, and handbells, and robotics (VEX).
+      </h2>
+    </div>
+  </div>
+@stop
