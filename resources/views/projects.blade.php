de08f474c7115043367c9f8619500215dbe6c77f
Bill Z. Qin <bzqin88@gmail.com>
2020-02-02 19:53:10 -0500

    fadeboxes on projects

--- resources/views/projects.blade.php
+++ resources/views/projects.blade.php
@@ -40,6 +40,14 @@
       @endphp
       ><svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 24 24"><path d="M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z"/></svg></button></div>
     </div>
+    <div id = "right-hide">
+      <div id = "right-grad-box"></div>
+      <div id = "right-hide-box"></div>
+    </div>
+    <div id = "left-hide">
+      <div id = "left-hide-box"></div>
+      <div id = "left-grad-box"></div>
+    </div>
     <div id = "loading-text"><h2>Loading assets...</h2></div>
     <div class = "scrollable-box" id = "projects-box">
       @foreach($projects["projects"] as $proj)
